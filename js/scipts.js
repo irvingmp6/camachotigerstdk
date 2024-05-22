b4770c1e3624371f06970ff544ccc8349bcacc36
Irving Martinez <irvingmp6@hotmail.com>
2024-05-22 11:56:13 -0500

    complete rewrite

--- js/scipts.js
+++ js/scipts.js
@@ -1,19 +1,33 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function () {
-    const enElements = document.querySelectorAll('.lang-en');
-    const esElements = document.querySelectorAll('.lang-es');
-
-    document.getElementById('lang-en').addEventListener('click', function () {
-        enElements.forEach(el => el.style.display = 'block');
-        esElements.forEach(el => el.style.display = 'none');
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+    
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+  
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
     });
+  })
 
-    document.getElementById('lang-es').addEventListener('click', function () {
-        enElements.forEach(el => el.style.display = 'none');
-        esElements.forEach(el => el.style.display = 'block');
-    });
-
-    // Default language
-    enElements.forEach(el => el.style.display = 'block');
-    esElements.forEach(el => el.style.display = 'none');
+  $(document).ready(function() {
+    var navbarHeight = $('.navbar').outerHeight();
+    $('body').css('padding-top', navbarHeight + 'px');
 });
